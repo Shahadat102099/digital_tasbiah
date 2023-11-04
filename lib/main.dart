3045eb489b58284d4d2db3b80c7b96bf3167b654
SHAHADAT <shahadatakashs102099@gmail.com>
2023-11-04 16:30:24 +0530

    Assignment Done

--- lib/main.dart
+++ lib/main.dart
@@ -10,94 +10,116 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WeatherApp(),
+      debugShowCheckedModeBanner: false,
+      title: 'Photo Gallery App',
+      home: PhotoList(),
     );
   }
 }
 
-class WeatherApp extends StatefulWidget {
-  @override
-  _WeatherAppState createState() => _WeatherAppState();
+class Photo {
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Photo({
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      id: json['id'],
+      title: json['title'],
+      url: json['url'],
+      thumbnailUrl: json['thumbnailUrl'],
+    );
+  }
 }
 
-class _WeatherAppState extends State<WeatherApp> {
-  List<Map<String, dynamic>> citiesData = [];
+class PhotoList extends StatefulWidget {
+  @override
+  _PhotoListState createState() => _PhotoListState();
+}
+
+class _PhotoListState extends State<PhotoList> {
+  List<Photo> photos = [];
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    String jsonData = '''
-      [
-        {
-          "city": "New York",
-          "temperature": 20,
-          "condition": "Clear",
-          "humidity": 60,
-          "windSpeed": 5.5
-        },
-        {
-          "city": "Los Angeles",
-          "temperature": 25,
-          "condition": "Sunny",
-          "humidity": 50,
-          "windSpeed": 6.8
-        },
-        {
-          "city": "London",
-          "temperature": 15,
-          "condition": "Partly Cloudy",
-          "humidity": 70,
-          "windSpeed": 4.2
-        },
-        {
-          "city": "Tokyo",
-          "temperature": 28,
-          "condition": "Rainy",
-          "humidity": 75,
-          "windSpeed": 8.0
-        },
-        {
-          "city": "Sydney",
-          "temperature": 22,
-          "condition": "Cloudy",
-          "humidity": 55,
-          "windSpeed": 7.3
-        }
-      ]
-    ''';
+    fetchPhotos();
+  }
 
-    List<dynamic> jsonDataList = json.decode(jsonData);
+  Future<void> fetchPhotos() async {
+    final url = Uri.parse('https://jsonplaceholder.typicode.com/photos');
 
-    setState(() {
-      citiesData = jsonDataList.cast<Map<String, dynamic>>();
-    });
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonList = json.decode(response.body);
+        setState(() {
+          photos = jsonList.map((json) => Photo.fromJson(json)).toList();
+          isLoading = false;
+        });
+      } else {
+        throw Exception('Failed to load photos');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Weather Info App'),
+        title: Text('Photo Gallery App'),
       ),
-      body: ListView.builder(
-        itemCount: citiesData.length,
-        itemBuilder: (BuildContext context, int index) {
-          var cityData = citiesData[index];
-          return Card(
-            child: ListTile(
-              title: Text(cityData['city']),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Temperature: ${cityData['temperature']}°C'),
-                  Text('Condition: ${cityData['condition']}'),
-                  Text('Humidity: ${cityData['humidity']}%'),
-                  Text('Wind Speed: ${cityData['windSpeed']} km/h'),
-                ],
-              ),
-            ),
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: photos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(photos[index].title),
+            leading: Image.network(photos[index].thumbnailUrl),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PhotoDetail(photo: photos[index]),
+                ),
+              );
+            },
           );
         },
+      ),
+    );
+  }
+}
+
+class PhotoDetail extends StatelessWidget {
+  final Photo photo;
+
+  PhotoDetail({required this.photo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Photo Detail'),
+      ),
+      body: Column(
+        children: [
+          Image.network(photo.url),
+          Text('ID: ${photo.id}'),
+          Text('Title: ${photo.title}'),
+        ],
       ),
     );
   }
